c1f7bafbcf01f264307d69ff3436496aa5680cb1
jhenriquedsm <jhsilvamata@gmail.com>
2023-10-10 22:23:18 -0300

    Adição da dependência Lombok e criação de Getters and Setters da classe UserModel

--- src/main/java/jhenriquedsm/todolist/user/UserModel.java
+++ src/main/java/jhenriquedsm/todolist/user/UserModel.java
@@ -1,18 +1,16 @@
 package jhenriquedsm.todolist.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data // Getters and Setters
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class UserModel {
     private String username;
     private String name;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getPassword() {
-        return password;
-    }
-    
 }
