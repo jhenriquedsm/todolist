0be61a84987b6188dd7e1250040b9e37858a66e8
jhenriquedsm <jhsilvamata@gmail.com>
2023-10-10 23:03:52 -0300

    Criação do Repository e tratamento/validação ao criar usuario no banco de dados

--- src/main/java/jhenriquedsm/todolist/user/UserController.java
+++ src/main/java/jhenriquedsm/todolist/user/UserController.java
@@ -1,5 +1,9 @@
 package jhenriquedsm.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
     
+    @Autowired
+    private IUserRepository repository;
+
     @PostMapping("/create")
-    public void createUser(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getName());
+    public ResponseEntity createUser(@RequestBody UserModel userModel) {
+        var user = this.repository.findByUsername(userModel.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+        var userCreated = this.repository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
