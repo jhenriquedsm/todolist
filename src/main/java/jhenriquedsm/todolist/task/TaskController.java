f6be966758f799555b0ec46598af6310051285a6
jhenriquedsm <jhsilvamata@gmail.com>
2023-10-13 17:01:47 -0300

    Validando usuário dono

--- src/main/java/jhenriquedsm/todolist/task/TaskController.java
+++ src/main/java/jhenriquedsm/todolist/task/TaskController.java
@@ -51,12 +51,24 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
-        var idUser = request.getAttribute("idUser");
-        
+    public ResponseEntity update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request){
         var task = this.repository.findById(id).orElse(null);
+
+        if(task == null){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+             .body("Tarefa não encontrada!");
+        }
+
+        var idUser = request.getAttribute("idUser");
+
+        if(!task.getIdUser().equals(idUser)){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+             .body("Usuário não tem permissão para alterar essa tarefa");
+        }
+
         Utils.copyNonNullProperties(taskModel, task);
 
-        return this.repository.save(task);
+        var taskUpdated = this.repository.save(task);
+        return ResponseEntity.ok().body(taskUpdated);
     }
 }
